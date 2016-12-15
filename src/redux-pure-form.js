b52eb0a4173bd4bea3652f8e54591e4812889b2d
Max Mykhailenko <max.mykhailenko@gmail.com>
2016-12-15 17:24:41 +0200

    chore: v1.2.9

--- src/redux-pure-form.js
+++ src/redux-pure-form.js
@@ -1,4 +1,9 @@
-export * as formActionTypes from './action-types.js';
-export * as formActions from './actions.js';
-export mergeActionsToProps from './attributes.js';
-export formReducer from './reducer.js';
+import * as types from './action-types.js';
+import * as actions from './actions.js';
+import mergeProps from './attributes.js';
+import reducer from './reducer.js';
+
+export { types as formActionTypes };
+export { actions as formActions };
+export { mergeProps as mergeActionsToProps };
+export { reducer as formReducer };
