4be0344e188618e29bf282856cf096e6b56b107e
kramarmm <kramar.makra@gmail.com>
2017-06-22 07:03:50 +0300

    tests are updated

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -1,6 +1,12 @@
 /* eslint-disable */
-import * as actions from './actions.js';
+import { FIELD_ON_CHANGE } from '../src/action-types.js';
+import * as actions from '../src/actions.js';
 
-it('create action object', () => {
-
+it('return action object with right type', () => {
+    const fields = { 'user.name': 'superman' };
+    const expectedAction = {
+      type: FIELD_ON_CHANGE,
+      payload: fields,
+    }
+    expect(actions.onChange(fields)).toEqual(expectedAction);
 });
--- __tests__/redusers.js
+++ __tests__/redusers.js
@@ -1,6 +1,24 @@
 /* eslint-disable */
-import reducer from './reducer.js';
+import { FIELD_ON_CHANGE } from '../src/action-types.js';
+import reducer from '../src/reducer.js';
 
-it('return updated state', () => {
+describe('reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer('user')(undefined, {})).toEqual({});
+  });
 
+  it('should handle FIELD_ON_CHANGE', () => {
+    expect(
+      reducer('user')({}, {
+        type: FIELD_ON_CHANGE,
+        payload: { 'user.name': 'superman' },
+      })
+    ).toEqual(
+      {
+        user: {
+          name: 'superman',
+        }
+      }
+    );
+  });
 });
