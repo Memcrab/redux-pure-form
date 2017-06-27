d7a8bce4abcc0d58601c3c4fb880da9fbdcba578
kramarmm <kramar.makra@gmail.com>
2017-06-27 18:56:04 +0300

    enable eslint with globals for testing

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -1,12 +1,12 @@
-/* eslint-disable */
+/* global describe it expect */
 import { FIELD_ON_CHANGE } from '../src/action-types.js';
 import * as actions from '../src/actions.js';
 
 it('return action object with right type', () => {
-    const fields = { 'user.name': 'superman' };
-    const expectedAction = {
-      type: FIELD_ON_CHANGE,
-      payload: fields,
-    }
-    expect(actions.onChange(fields)).toEqual(expectedAction);
+  const fields = { 'user.name': 'superman' };
+  const expectedAction = {
+    type: FIELD_ON_CHANGE,
+    payload: fields,
+  };
+  expect(actions.onChange(fields)).toEqual(expectedAction);
 });
